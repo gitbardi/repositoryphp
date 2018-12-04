2c06be9a5821bb7b1c6267d7fa1cf1942c1f53d0
bardiphp@gmail.com <bardiphp@gmail.com>
2018-12-04 11:18:02 +0100

    MODIFICA db preprod

--- index.php
+++ index.php
@@ -97,5 +97,6 @@ and open the template in the editor.
 </section>
        <script src='js/bootstrap.min.js'></script>
         <script src='js/jquery.min.js'></script>
+        
     </body>
 </html>
